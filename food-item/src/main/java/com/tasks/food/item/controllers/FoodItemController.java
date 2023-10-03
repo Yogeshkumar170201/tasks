3377ca866a3cd6eaa31ce5170eb5b3e74db3e432
Yogeshkumar170201 <yogeshkumar170201@gmail.com>
2023-10-03 07:49:43 +0000

    Update FoodItemController.java for different logging levels

--- food-item/src/main/java/com/tasks/food/item/controllers/FoodItemController.java
+++ food-item/src/main/java/com/tasks/food/item/controllers/FoodItemController.java
@@ -2,12 +2,14 @@ package com.tasks.food.item.controllers;
 
 import com.tasks.food.item.models.FoodItem;
 import com.tasks.food.item.repository.FoodItemRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Log4j2
 public class FoodItemController {
 
     @Autowired
@@ -15,6 +17,12 @@ public class FoodItemController {
 
     @PostMapping("/addItem")
     public int addItem(@RequestBody FoodItem foodItem){
+        log.trace("This is a TRACE log");
+        log.debug("This is a DEBUG log");
+        log.info("This is an INFO log");
+        log.error("This is an ERROR log");
+        log.warn("This is an warn log");
+        log.fatal("This is an fatal log");
         foodItemRepository.addItem(foodItem);
         return foodItem.getId();
     }
